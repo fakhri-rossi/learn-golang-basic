aed3f6aa6411e1a0c2f6d7d8dd70ad672f87ef1d
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-14 07:53:19 +0700

    Constant

---
+++ constant.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName = "Fakhri"
+	const lastName = "Rossi"
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	const (
+		student1 = "Rossi"
+		student2 = "Budi"
+	)
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+}
