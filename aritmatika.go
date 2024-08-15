bc60ebebcbdf67cfe05ff11c539ab300d471f159
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-15 07:28:48 +0700

    Operasi Aritmatika

---
+++ aritmatika.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 10
+	c := a + b
+
+	fmt.Println(c)
+
+	//	u can use augmented assignments
+	var i = 10
+	i += 5
+	fmt.Println(i)
+
+	//	unary operator
+	j := 1
+	j++
+	fmt.Println(j)
+
+}
